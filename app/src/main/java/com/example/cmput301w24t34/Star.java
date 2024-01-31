f14e1ec9dd8a750d1da10d71c089a8ee94f2f4cb
Aaryan Singh <aaryan.jsingh@gmail.com>
2024-01-31 15:55:33 -0700

    Cleaned the project

--- app/src/main/java/com/example/cmput301w24t34/Star.java
+++
@@ -1,6 +0,0 @@
-package com.example.cmput301w24t34;
-
-public class Star extends Shape{
-    public Star() {
-    }
-}
--- app/src/main/java/com/example/cmput301w24t34/Triangle.java
+++
@@ -1,6 +0,0 @@
-package com.example.cmput301w24t34;
-
-public class Triangle extends Shape{
-    public Triangle() {
-    }
-}
