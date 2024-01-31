8c08f3448b0bbc446cc6954758feee474764acb2
Alyse Ridge <94921692+aridge03@users.noreply.github.com>
2024-01-30 17:51:48 -0700

    Fixed directory commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CMPUT301W24T34"
+include(":app")
+ 
